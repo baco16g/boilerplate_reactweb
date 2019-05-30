33df048c5dd3edc08ebe018ba7b730e8ee9a6e85
baco16g <baco.16g@gmail.com>
2019-05-30 14:50:03 +0900

    fix: fix a bug for eslint

--- config/path.js
+++ config/path.js
@@ -1,13 +1,13 @@
-const path = require('path')
+const path = require('path');
 
-const ROOT = path.join(__dirname, '../')
-const STATIC = path.resolve(`${ROOT}/static`)
-const SRC = path.resolve(`${ROOT}/src`)
-const DEST = path.resolve(`${ROOT}/dist`)
+const ROOT = path.join(__dirname, '../');
+const STATIC = path.resolve(`${ROOT}/static`);
+const SRC = path.resolve(`${ROOT}/src`);
+const DEST = path.resolve(`${ROOT}/dist`);
 
-exports.ROOT = ROOT
-exports.SRC = SRC
-exports.DEST = DEST
-exports.TEMPLATE_SRC = path.resolve(`${SRC}/templates`)
-exports.JS_SRC = path.resolve(`${SRC}/scripts`)
-exports.ENTRY_POINTS = path.resolve(`${SRC}/scripts/entrypoints`)
+exports.ROOT = ROOT;
+exports.SRC = SRC;
+exports.DEST = DEST;
+exports.TEMPLATE_SRC = path.resolve(`${SRC}/templates`);
+exports.JS_SRC = path.resolve(`${SRC}/scripts`);
+exports.ENTRY_POINTS = path.resolve(`${SRC}/scripts/entrypoints`);
--- webpack/utils/getEntries.js
+++ webpack/utils/getEntries.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
+const fs = require('fs');
 
-module.exports = function (path, extensions) {
+module.exports = function(path, extensions) {
   return fs
     .readdirSync(path)
     .filter(file => file.match(/.*\.(js|jsx|ts|tsx)$/))
     .map(file => ({
       name: file.replace(/\.[^/.]+$/, ''),
-      path: `${path}/${file}`
+      path: `${path}/${file}`,
     }))
     .reduce((acc, file) => {
-      acc[file.name] = file.path
-      return acc
-    }, extensions)
-}
+      acc[file.name] = file.path;
+      return acc;
+    }, extensions);
+};
