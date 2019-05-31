e997710a7fd6d02144db484db3890296bffc26f9
baco16g <baco.16g@gmail.com>
2019-05-31 13:53:14 +0900

    test: add test cases for Button

--- src/scripts/views/shared/Button.tsx
+++ src/scripts/views/shared/Button.tsx
@@ -3,29 +3,30 @@ import styled from 'styled-components';
 
 interface Props {
   fontsize?: number;
+  color?: string;
   label: string;
   onPress: (event: React.MouseEvent) => void;
 }
 
-const Button = ({label, onPress, fontsize = 12}: Props) => {
+const Button = ({label, onPress, fontsize = 12, color = '#67c5ff'}: Props) => {
   return (
-    <StyledButton fontsize={fontsize} onClick={onPress}>
+    <StyledButton fontsize={fontsize} color={color} onClick={onPress}>
       {label}
     </StyledButton>
   );
 };
 
-const StyledButton = styled.button<{fontsize: number}>`
+const StyledButton = styled.button<{fontsize: number; color: string}>`
   display: inline-block;
   padding: 0.3em 1em;
   text-decoration: none;
   font-size: ${props => `${props.fontsize}px`}
-  color: #67c5ff;
-  border: solid 2px #67c5ff;
+  color: ${props => props.color};
+  border: solid 2px ${props => props.color};
   border-radius: 3px;
   transition: .4s;
   &:hover {
-    background: #67c5ff;
+    background: ${props => props.color};
     color: white;
   }
 `;
