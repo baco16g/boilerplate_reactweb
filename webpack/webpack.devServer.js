51b761bc39c8d3f3a313c4381fdd7854c86d0d21
baco16g <baco.16g@gmail.com>
2019-05-31 12:47:39 +0900

    build: add a option is historyApiFallback to webpack's config.

--- webpack/webpack.devServer.js
+++ webpack/webpack.devServer.js
@@ -7,6 +7,7 @@ module.exports = {
   port: 3000,
   hot: true,
   hotOnly: true,
+  historyApiFallback: true,
   contentBase: path.join(__dirname, '../dist'),
   before: app => {
     app.use('/static', express.static(path.resolve(__dirname, '../static')));
