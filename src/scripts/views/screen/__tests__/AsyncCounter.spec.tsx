5b60e8755b15b9506201de27bb084a56a40bd701
baco16g <baco.16g@gmail.com>
2019-05-31 12:44:20 +0900

    test: change test cases for async couter. the elapsed time is taken into consideration.

--- src/scripts/views/screen/__tests__/AsyncCounter.spec.tsx
+++ src/scripts/views/screen/__tests__/AsyncCounter.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {render, cleanup, fireEvent, waitForElement} from 'react-testing-library';
+import {render, cleanup, fireEvent, waitForDomChange} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import 'jest-styled-components';
 import AsyncCounter, {STORAGE_KEY as COUNTER_STORAGE_KEY} from '../AsyncCounter';
@@ -18,17 +18,23 @@ describe('AsyncCounter', () => {
     expect(count).toHaveTextContent('0');
   });
 
-  it('should count up by one, when you press the [ASYNC INCREMENT] button', async () => {
+  it('should count up by one after about 1000ms, when you press the [ASYNC INCREMENT] button', async () => {
     const {getByText} = render(<AsyncCounter />);
-    fireEvent.click(getByText('ASYNC INCREMENT'));
-    const count = await waitForElement(() => getByText('Count: 1'));
+    fireEvent.click(getByText('ASYNC INCREMENT')!);
+    const count = getByText(/Count: \d+/);
+    const startMs = new Date().getTime();
+    await waitForDomChange({container: count});
+    const elapsedMs = new Date().getTime() - startMs;
     expect(count).toHaveTextContent('1');
+    expect(elapsedMs).toBeGreaterThanOrEqual(1000);
+    expect(elapsedMs).toBeLessThan(1100);
   });
 
   test('should save a count to sessionStorage', async () => {
     const {getByText} = render(<AsyncCounter />);
-    fireEvent.click(getByText('ASYNC INCREMENT'));
-    const count = await waitForElement(() => getByText('Count: 1'));
+    const count = getByText(/Count: \d+/);
+    fireEvent.click(getByText('ASYNC INCREMENT')!);
+    await waitForDomChange({container: count});
     expect(count).toHaveTextContent('1');
     fireEvent.click(getByText('SAVE'));
     expect(sessionStorage.__STORE__[COUNTER_STORAGE_KEY]).toBe(JSON.stringify({count: 1}));
