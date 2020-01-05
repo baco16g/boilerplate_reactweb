ccfc437d3422a92d6c3909402f1b921d29c7907c
baco16g <baco.16g@gmail.com>
2020-01-05 20:06:58 +0900

    change routes for Github Page

--- src/scripts/routers/routes.ts
+++ src/scripts/routers/routes.ts
@@ -1,13 +1,22 @@
 import * as React from "react";
 import { Routes } from "universal-router";
 
+const publicPath = (() => {
+  switch (process.env.ENV ) {
+    case 'GITHUB_PAGES':
+      return '/boilerplate_reactweb'
+    default:
+      return ''
+  }
+})()
+
 const routes: Routes<any, { default: React.ComponentType }> = [
   {
-    path: "/",
+    path: `${publicPath}/`,
     action: () => import("../views/screen/Counter"),
   },
   {
-    path: "/async",
+    path: `${publicPath}/async`,
     action: () => import("../views/screen/AsyncCounter"),
   },
 ];
